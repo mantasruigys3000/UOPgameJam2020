c270bd165da559d9b6418f72e3e063d59eae6dd2
Teodor Sava <mice.graff@gmail.com>
2020-06-17 15:22:51 +0100

    menu done

---
+++ Curling Game/Assets/Scripts/facingScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class facingScript : MonoBehaviour
+{
+    public Camera cam;
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mouse_pos;
+        Transform target = gameObject.transform; //Assign to the object you want to rotate
+        Vector3 object_pos;
+        mouse_pos = Input.mousePosition;
+        mouse_pos.z = 5.23f; //The distance between the camera and object
+        object_pos = cam.WorldToScreenPoint(target.position);
+        mouse_pos.x = mouse_pos.x - object_pos.x;
+        mouse_pos.y = mouse_pos.y - object_pos.y;
+        float angle;
+        angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
+        Debug.Log(angle);
+        if (angle>= 0 && angle<= 90)
+        {
+            anim.Play("Idle");
+        }
+        else if(angle>=91 && angle<=180)
+        {
+            anim.Play("Idle");
+            gameObject.transform.rotation = new Quaternion(
+    gameObject.transform.rotation.x,
+    180f,
+    gameObject.transform.rotation.z,0
+);
+        }
+    }
+}
---
+++ Curling Game/Assets/Scripts/menuScript.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuScript : MonoBehaviour
+{
+    public GameObject start;
+    public GameObject controls;
+    public GameObject settings;
+    public GameObject exit;
+
+    public Camera cam;
+
+    public AudioSource sound;
+
+    public int counter=0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown("s"))
+        {
+            sound.Play();
+            counter++;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown("w"))
+        {
+            sound.Play();
+            counter--;
+        }
+
+        if (counter>= 4)
+        {
+            counter = 0;
+        }
+        else if (counter<= -1)
+        {
+            counter = 3;
+        }
+
+        switch (counter)
+        {
+            case 0:
+                start.GetComponent<Animator>().Play("button");
+                controls.GetComponent<Animator>().Play("no");
+                settings.GetComponent<Animator>().Play("no");
+                exit.GetComponent<Animator>().Play("no");
+                break;
+            case 1:
+                controls.GetComponent<Animator>().Play("button");
+                start.GetComponent<Animator>().Play("no");
+                settings.GetComponent<Animator>().Play("no");
+                exit.GetComponent<Animator>().Play("no");
+                break;
+            case 2:
+                settings.GetComponent<Animator>().Play("button");
+                controls.GetComponent<Animator>().Play("no");
+                start.GetComponent<Animator>().Play("no");
+                exit.GetComponent<Animator>().Play("no");
+                break;
+            case 3:
+                exit.GetComponent<Animator>().Play("button");
+                controls.GetComponent<Animator>().Play("no");
+                settings.GetComponent<Animator>().Play("no");
+                start.GetComponent<Animator>().Play("no");
+                break;
+        }
+
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+        RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+        if (hit.collider!= null)
+        {
+            sound.Play();
+            if (hit.collider.gameObject.name == "Start")
+            {
+                counter = 0;
+            }
+            else if (hit.collider.gameObject.name == "Controlls")
+            {
+                counter = 1;
+            }
+            else if (hit.collider.gameObject.name == "Settings")
+            {
+                counter = 2;
+            }
+            else if (hit.collider.gameObject.name == "exit")
+            {
+                counter = 3;
+            }
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Return) || Input.GetMouseButton(0))
+        {
+            switch (counter)
+            {
+                case 0:
+                    SceneManager.LoadScene(1);
+                    break;
+                case 1:
+                    SceneManager.LoadScene(2);
+                    break;
+                case 2:
+                    SceneManager.LoadScene(3);
+                    break;
+                case 3:
+                    Application.Quit();
+                    break;
+            }
+            
+        }
+
+    }
+}
